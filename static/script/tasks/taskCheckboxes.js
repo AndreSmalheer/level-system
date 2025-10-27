cb8f2db50351b936f0c21eace7a1698110387782
AndreSmalheer <driekie248@gmail.com>
2025-10-27 13:02:25 +0100

    time styling added compleet sty;ling added

--- static/script/tasks/taskCheckboxes.js
+++ static/script/tasks/taskCheckboxes.js
@@ -5,6 +5,16 @@ export function initCheckboxes() {
     checkbox.addEventListener("change", (event) => {
       const taskLabel = event.target.closest(".task_label");
       if (!taskLabel) return;
+
+      const task = taskLabel.closest(".task");
+      if (!task.classList.contains("failed")) {
+        if (checkbox.checked) {
+          task.classList.add("completed");
+        } else {
+          task.classList.remove("completed");
+        }
+      }
+
       updateRewards(taskLabel, checkbox.checked);
     });
   });
