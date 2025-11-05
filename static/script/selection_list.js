63864c7418ae60835f11c742fee1386d83975805
102464-AS <102464@glr.nl>
2025-11-05 13:41:42 +0100

    add concequense button added with styling

--- static/script/selection_list.js
+++ static/script/selection_list.js
@@ -1,6 +1,8 @@
 const listItems = document.querySelectorAll("#selection_list li");
 const task_container = document.getElementById("tasks_container");
 const consequences = document.getElementById("consequences_container");
+const add_consequence_btn = document.getElementById("add_consequence_btn");
+const add_task_btn = document.getElementById("add_task_btn");
 
 listItems.forEach((item) => {
   item.addEventListener("click", () => {
@@ -11,14 +13,18 @@ listItems.forEach((item) => {
     // Hide all containers
     task_container.style.display = "none";
     consequences.style.display = "none";
+    add_task_btn.style.display = "none";
+    add_consequence_btn.style.display = "none";
 
     // Show the right container
     if (item.textContent.trim() == "Consequences") {
       consequences.style.display = "flex";
+      add_consequence_btn.style.display = "block";
     }
 
     if (item.textContent.trim() === "Tasks") {
       task_container.style.display = "flex";
+      add_task_btn.style.display = "block";
     }
   });
 });
