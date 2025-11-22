e74992c4f80c4db32d72a7ded8d4057a6b1bbf55
102464-AS <102464@glr.nl>
2025-11-22 19:02:57 +0100

    pop up is now unavalible on failed task

--- static/script/modules/tasks.js
+++ static/script/modules/tasks.js
@@ -120,12 +120,38 @@ class PopUp {
 
     this.element.classList.remove("hide");
     this.element.classList.add("show");
+
+    const parentElement = document.getElementById(this.currentItemId);
+
+    // Clear existing content first
+    this.element.innerHTML = "";
+
+    if (parentElement.classList.contains("failed")) {
+      this.element.innerHTML = `<ul><li>unavailable</li></ul>`;
+    } else {
+      if (this.items.length > 0) {
+        const ul = document.createElement("ul");
+        for (let item of this.items) {
+          const li = document.createElement("li");
+          li.textContent = item;
+
+          li.addEventListener("click", () => {
+            this.LiCLick(item);
+            this.hide();
+          });
+
+          ul.appendChild(li);
+        }
+        this.element.appendChild(ul); // use this.element here
+      }
+    }
   }
 
   hide() {
     this.element.classList.remove("show");
     this.element.classList.add("hide");
   }
+
   LiCLick(item) {
     const action = item.toLowerCase();
     handleLiAction(action, this.currentItemId, this.currentItemType);
